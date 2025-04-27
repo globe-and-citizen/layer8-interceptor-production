1df6f175c9f3b00fb228987378fc67400e477c1e
thaodtp <dtpthao.its@gmail.com>
2025-04-28 01:08:37 +0700

    add mocked functions

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -6,4 +6,22 @@ import * as interceptor_wasm from 'interceptor-wasm'
 
 createApp(App).mount('#app')
 
-interceptor_wasm.greet()
+interceptor_wasm.test_wasm()
+interceptor_wasm.persistence_check()
+interceptor_wasm.check_encrypted_tunnel().then(val => {
+    console.log("Encrypted tunnel check result: ", val)
+})
+
+interceptor_wasm.init_encrypted_tunnel({"hello": "world"}).then(val => {
+    console.log("Encrypted tunnel init result: ", val)
+})
+
+interceptor_wasm.fetch("hello").then(val => {
+    console.log("Encrypted tunnel fetch result: ", val)
+})
+
+interceptor_wasm.get_static("hello").then(val => {
+    console.log("Static fetch result: ", val)
+}).catch(err => {
+    console.error("Static fetch error: ", err)
+})
