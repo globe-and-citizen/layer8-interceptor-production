9d109cb5c84f23931e40b5f24247f1ed563d785b
Osoro Bironga <fanosoro@gmail.com>
2025-12-02 16:28:02 +0300

    provide bincode serde

--- src/types/mod.rs
+++ src/types/mod.rs
@@ -5,10 +5,3 @@ pub mod network_state;
 pub mod request;
 mod response;
 pub(crate) mod service_provider;
-
-/// this struct will be replaced by the WasmEncryptedMessage struct from ntor repository when available
-#[derive(Clone, Serialize, Deserialize, Debug)]
-pub struct WasmEncryptedMessage {
-    pub nonce: Vec<u8>,
-    pub data: Vec<u8>,
-}
